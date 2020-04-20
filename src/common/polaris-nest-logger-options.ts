2e1bf7356ede6ae91991e5ee74c5d1cb386afdf2
Yarin <yarinvak@gmail.com>
2020-04-20 16:18:48 +0300

    fix: multiple fixes and additions

--- src/common/polaris-nest-logger-options.ts
+++ src/common/polaris-nest-logger-options.ts
@@ -1,6 +1,12 @@
 import {ApplicationProperties, LoggerConfiguration} from "@enigmatis/polaris-logs";
+import {ModuleMetadata} from "@nestjs/common/interfaces";
 
 export interface PolarisNestLoggerOptions {
-    applicationProperties: ApplicationProperties,
-    loggerConfiguration: LoggerConfiguration
+    applicationProperties: ApplicationProperties;
+    loggerConfiguration: LoggerConfiguration;
+}
+
+export interface PolarisNestLoggerAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    useFactory: (...args: any[]) => Promise<PolarisNestLoggerOptions> | PolarisNestLoggerOptions;
+    inject? : any[];
 }
--- src/polaris-nest-logger-module.ts
+++ src/polaris-nest-logger-module.ts
@@ -1,7 +1,7 @@
-import {DynamicModule, Global, Module} from "@nestjs/common";
+import {DynamicModule, Global, Module, Provider} from "@nestjs/common";
 import {PolarisNestLogger} from "./services/polaris-nest-logger";
 import {PolarisLoggerFactory} from "./services/polaris-logger-factory.service";
-import {PolarisNestLoggerOptions} from "./common/polaris-nest-logger-options";
+import {PolarisNestLoggerAsyncOptions, PolarisNestLoggerOptions} from "./common/polaris-nest-logger-options";
 import {LoggerOptionsToken} from "./common/constants";
 
 @Global()
@@ -13,6 +13,23 @@ export class PolarisNestLoggerModule {
             providers: [{provide: LoggerOptionsToken, useValue: options},
                 PolarisNestLogger, PolarisLoggerFactory],
             exports: [PolarisNestLogger]
+        };
+    }
+
+    static registerAsync(options: PolarisNestLoggerAsyncOptions): DynamicModule {
+        return {
+            module: PolarisNestLoggerModule,
+            providers: [PolarisNestLogger, PolarisLoggerFactory, this.createConfigurationProvider(options)],
+            imports: options.imports,
+            exports: [PolarisNestLogger]
+        }
+    }
+
+    private static createConfigurationProvider(options: PolarisNestLoggerAsyncOptions): Provider {
+        return {
+            provide: LoggerOptionsToken,
+            useFactory: options.useFactory,
+            inject: options.inject,
         };
     }
 }
