f1819a63b1f8c4888e7ba75c7bacb19441a5d905
Yarin <yarinvak@gmail.com>
2020-04-20 14:48:34 +0300

    fix: add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export {PolarisNestLoggerModule} from './polaris-nest-logger-module';
+export {PolarisNestLogger} from './services/polaris-nest-logger';
+export {PolarisNestLoggerOptions} from './common/polaris-nest-logger-options';
